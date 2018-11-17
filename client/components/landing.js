b17d416027a6cbc3320604c47bddfc447bd85b4a
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-11-17 17:58:53 -0500

    commit for presentation

--- client/components/landing.js
+++ client/components/landing.js
@@ -5,7 +5,9 @@ class Landing extends React.Component {
 	constructor(){
 		super()
 		this.state = {
-			title: 'Waiting...'
+			title: 'Waiting...',
+			intro: '...words...',
+			content: '...loading...'
 		}
 		this.getLocale = this.getLocale.bind(this)
 	}
@@ -15,17 +17,18 @@ class Landing extends React.Component {
 	}
 
 	async getLocale(){
-		const data = {gb: {title: 'Hallo Welt'}}
+		const data = {gb: {title: 'Hallo Welt!', intro: 'Und jetzt höre diese Worte der Weisheit...', content: 'Nehmen Sie die Krone ab. Soziale Netzwerke bestimmen nicht das Kaliber einer Person. Du sagst, ich war Reggaeton. Sag auch, dass ich Tango war. Bossa Nova, Cumbia, Batucada, Mambo. Fusion ist das Konzept!'}}
 		const result = await util(data)
-	
-		this.setState({title: result.title})
+		this.setState({title: result.title, intro: result.intro, content: result.content})
 	}
 
 	render(){
-		console.log(window.navigator.language)
 		return (
 		<div>
-			<h1>{this.state.title}</h1>
+			<h1 className="greeting">{this.state.title}</h1>
+			<h5 className="intro">{this.state.intro}</h5>
+			<h4 className="quote">"{this.state.content}"</h4>
+			<h5 className="close">-Residente</h5>
 		</div>
 		)
 	}
--- client/util.js
+++ client/util.js
@@ -3,15 +3,16 @@ import axios from 'axios'
 
 const main = async (data) => {
 	const langs = Object.keys(data)
-	const langIncluded = langs.includes(window.navigator.language.slice(0,2))
+	const langIncluded = langs.includes(window.navigator.language.slice(0, 2))
 	console.log('langs', langs, langIncluded)
-	 
-	if(langIncluded) return new LocalizedStrings(data)
+
+	if (langIncluded) {return new LocalizedStrings(data)}
 	else {
-		const res = await axios.post('api/lang', {data: data[langs[0]], target: window.navigator.language}).then(data => data.data)
-		data[window.navigator.language] = res
+		console.log(data[langs[0]], window.navigator.language, 'aquí')
+		const res = await axios.post('api/lang', {data: data[langs[0]], target: window.navigator.language.slice(0, 2)}).then(data => data.data)
+		data[window.navigator.language.slice(0, 2)] = res
 		console.log('response', res)
-		console.log('data',data)
+		console.log('data', data)
 		return new LocalizedStrings(data)
 	}
 }
