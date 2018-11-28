29b947e2e2814389dc66895ab082c6653b5afe65
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-11-28 15:12:11 -0500

    forgot to add this file to last commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ db.sync()
 		chalkAnimation.rainbow('Postgres server up and running...', 1.25)
 		app.listen(PORT, (err) => {
 			if (err) throw err
-			setTimeout( () => chalkAnimation.neon(`Listening on port ${PORT}.`), 1500 ) //the setTimeout is only necessary for the chalkAnimation to work accordingly
+			setTimeout( () => chalkAnimation.neon(`Listening on http://localhost:${PORT}`), 1500 ) //the setTimeout is only necessary for the chalkAnimation to work accordingly
 		})
 	})
 	.catch(console.error)
